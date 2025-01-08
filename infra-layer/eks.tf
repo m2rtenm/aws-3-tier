dbaae4a7f4ddca1971cbc3e6584254a8b3e4c352
Märten Mändla <marten.mandla@gmail.com>
2025-01-08 16:09:11 +0200

    EKS and RDS enhancements

--- infra-layer/eks.tf
+++ infra-layer/eks.tf
@@ -6,12 +6,35 @@ module "eks" {
   cluster_version = "1.31"
 
   cluster_addons = {
-    coredns = {}
-    eks-pod-identity-agent = {}
-    kube-proxy = {}
-    vpc-cni = {}
+    coredns = {
+      most_recent = true
+    }
+    eks-pod-identity-agent = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
   }
 
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access = false
+
+  eks_managed_node_groups = {
+    cluster_nodes = {  
+      ami_type = "AL2023_x86_64_STANDARD"
+      instance_types = ["t3.medium"]
+
+      min_capacity = 1
+      desired_capacity = 2
+      max_capacity = 3
+    }
+  }
 }
--- infra-layer/rds.tf
+++ infra-layer/rds.tf
@@ -3,6 +3,31 @@ module "rds_master" {
   version = "6.10.0"
   
   identifier = "${local.name}-rds-master"
+
+  engine = local.engine
+  engine_version = local.engine_version
+  family = local.family
+  major_engine_version = local.major_engine_version
+  instance_class = local.instance_class
+
+  allocated_storage = local.allocated_storage
+  max_allocated_storage = local.max_allocated_storage
+
+  db_name = "${local.name}-db"
+  username = "appdbuser"
+  port = local.port
+
+  multi_az = true
+  db_subnet_group_name = module.vpc.database_subnet_group_name
+  
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+
+  backup_retention_period = 1
+  skip_final_snapshot     = true
+  deletion_protection     = false
+  storage_encrypted       = false
 }
 
 module "rds_replica" {
@@ -10,4 +35,25 @@ module "rds_replica" {
   version = "6.10.0"
   
   identifier = "${local.name}-rds-replica"
+
+  replicate_source_db = module.rds_master.db_instance_arn
+
+  engine = local.engine
+  engine_version = local.engine_version
+  family = local.family
+  major_engine_version = local.major_engine_version
+  instance_class = local.instance_class
+
+  allocated_storage = local.allocated_storage
+  max_allocated_storage = local.max_allocated_storage
+
+  maintenance_window              = "Tue:00:00-Tue:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+
+  
+  port = local.port
+
+  multi_az = false
+  db_subnet_group_name = module.vpc.database_subnet_group_name
 }
